ac7372de17971602b9c7c1e2e44807b482e2af2f
silent-rain <2367221387@qq.com>
2024-02-19 00:33:41 +0800

    创建用户角色关联关系实体

--- server/core/migration/src/lib.rs
+++ server/core/migration/src/lib.rs
@@ -1,9 +1,10 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_table;
-mod m20230611_123914_create_perm_user_table;
 mod m20230617_084425_create_log_system_table;
+mod m20240216_123914_create_perm_user_table;
 mod m20240218_145452_create_perm_role_table;
+mod m20240218_161914_create_perm_user_role_rel;
 
 pub struct Migrator;
 
@@ -12,9 +13,10 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20220101_000001_create_table::Migration),
-            Box::new(m20230611_123914_create_perm_user_table::Migration),
+            Box::new(m20240216_123914_create_perm_user_table::Migration),
             Box::new(m20230617_084425_create_log_system_table::Migration),
             Box::new(m20240218_145452_create_perm_role_table::Migration),
+            Box::new(m20240218_161914_create_perm_user_role_rel::Migration),
         ]
     }
 }
---
+++ server/core/migration/src/m20240218_161914_create_perm_user_role_rel.rs
@@ -0,0 +1,70 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        manager
+            .create_table(
+                Table::create()
+                    .table(PermUserRoleRel::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(PermUserRoleRel::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key()
+                            .comment("ID"),
+                    )
+                    .col(
+                        ColumnDef::new(PermUserRoleRel::UserId)
+                            .integer()
+                            .not_null()
+                            .comment("用户ID"),
+                    )
+                    .col(
+                        ColumnDef::new(PermUserRoleRel::RoleId)
+                            .integer()
+                            .not_null()
+                            .comment("角色ID"),
+                    )
+                    .col(
+                        ColumnDef::new(PermUserRoleRel::CreatedAt)
+                            .date_time()
+                            .not_null()
+                            .comment("创建时间"),
+                    )
+                    .col(
+                        ColumnDef::new(PermUserRoleRel::UpdatedAt)
+                            .date_time()
+                            .not_null()
+                            .comment("更新时间"),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        manager
+            .drop_table(Table::drop().table(PermUserRoleRel::Table).to_owned())
+            .await
+    }
+}
+
+/// Learn more at https://docs.rs/sea-query#iden
+#[derive(Iden)]
+enum PermUserRoleRel {
+    #[iden = "perm_user_role_rel"]
+    Table,
+    Id,
+    UserId,
+    RoleId,
+    CreatedAt,
+    UpdatedAt,
+}
