4c16bb99ddb973da633ee18f1ce2943e49315dbe
silent-rain <2367221387@qq.com>
2024-03-24 15:32:15 +0800

    添加测试用例

---
+++ server/core/database/tests/common/user.rs
@@ -0,0 +1,18 @@
+use sea_orm::{
+    ActiveModelBehavior, DeriveEntityModel, DerivePrimaryKey, DeriveRelation, EntityTrait,
+    EnumIter, PrimaryKeyTrait,
+};
+
+#[derive(Clone, Default, Debug, PartialEq, DeriveEntityModel, Eq)]
+#[sea_orm(table_name = "user")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i32,
+    pub user_id: i32,
+    pub status: i8,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
---
+++ server/core/database/tests/sqlite_test.rs
@@ -0,0 +1,158 @@
+use std::env;
+
+mod common;
+use common::schem::create_user_table;
+use common::user;
+use common::User;
+
+use database::Curd;
+use database::DbRepo;
+use database::Pagination;
+use database::Pool;
+
+use sea_orm::{DbErr, EntityTrait, Set};
+
+async fn testcase(db: &Pool) -> Result<(), DbErr> {
+    let active_model = user::ActiveModel {
+        user_id: Set(2),
+        status: Set(1),
+        ..Default::default()
+    };
+
+    let result = User::insert(active_model)
+        .exec(db.wdb())
+        .await
+        .expect("could not insert baker");
+
+    assert_eq!(result.last_insert_id, 1);
+
+    Ok(())
+}
+
+struct UserDao<'a> {
+    db: &'a Pool,
+}
+
+impl<'a> Curd<user::Entity> for UserDao<'a> {
+    type Db = Pool;
+
+    fn db(&self) -> &Self::Db {
+        self.db
+    }
+}
+
+async fn test_all(db: &Pool) -> Result<(Vec<user::Model>, u64), DbErr> {
+    let dao = UserDao { db };
+    let (results, total) = dao.all().await.unwrap();
+    println!("test_all ======= {:#?}", results);
+    Ok((results, total))
+}
+
+async fn test_list(db: &Pool) -> Result<(Vec<user::Model>, u64), DbErr> {
+    let dao = UserDao { db };
+    let page = Pagination::new(0, 10);
+    let (results, total) = dao.list(page).await.unwrap();
+    println!("test_list ======= {:#?}", results);
+    Ok((results, total))
+}
+
+// async fn list_pages(db: &Pool) -> Result<(Vec<user::Model>, u64), DbErr> {
+//     let dao = UserDao { db };
+//     let page = Pagination::new(0, 10);
+//     let (results, total) = dao.list_pages(page).await.unwrap();
+//     println!("list_pages ======= {:#?}", results);
+//     Ok((results, total))
+// }
+
+async fn test_info(db: &Pool) -> Result<user::Model, DbErr> {
+    let dao = UserDao { db };
+    let result = dao.info(1).await.unwrap().unwrap();
+    println!("test_info ======= {:#?}", result);
+    Ok(result)
+}
+
+// async fn test_insert(db: &Pool) -> Result<user::Model, DbErr> {
+//     let dao = UserDao { db };
+//     let data = user::Model {
+//         user_id: 22,
+//         status: 1,
+//         ..Default::default()
+//     };
+//     let result = dao
+//         .insert::<user::ActiveModel>(data)
+//         .await
+//         .unwrap()
+//         .unwrap();
+//     println!("test_info ======= {:#?}", result);
+//     Ok(result)
+// }
+
+async fn test_insert2(db: &Pool) -> Result<user::Model, DbErr> {
+    let dao = UserDao { db };
+    let data = user::ActiveModel {
+        user_id: Set(22),
+        status: Set(1),
+        ..Default::default()
+    };
+    let result = dao.insert2(data).await.unwrap();
+    println!("test_insert2 ======= {:#?}", result);
+    Ok(result)
+}
+
+async fn test_update2(db: &Pool) -> Result<user::Model, DbErr> {
+    let dao = UserDao { db };
+    let data = user::ActiveModel {
+        id: Set(1),
+        user_id: Set(111),
+        ..Default::default()
+    };
+    let result = dao.update2(data).await.unwrap();
+    println!("test_update2 ======= {:#?}", result);
+    Ok(result)
+}
+
+async fn test_delete(db: &Pool) -> Result<u64, DbErr> {
+    let dao = UserDao { db };
+
+    let result = dao.delete(1).await.unwrap();
+    println!("test_delete ======= {:#?}", result);
+    Ok(result)
+}
+
+async fn test_batch_delete(db: &Pool) -> Result<u64, DbErr> {
+    let dao = UserDao { db };
+
+    let result = dao.batch_delete(vec![1, 2]).await.unwrap();
+    println!("test_batch_delete ======= {:#?}", result);
+    Ok(result)
+}
+
+// cargo test --package database --test sqlite_test -- --nocapture
+#[tokio::test]
+async fn main() -> Result<(), DbErr> {
+    env::set_var("RUST_BACKTRACE", "1");
+
+    // Connecting SQLite
+    let db_url = "sqlite::memory:".to_string();
+    let db = Pool::connect(db_url).await.expect("db init failed");
+    let pool = Pool::form_connect(db.clone(), db);
+
+    // Setup database schema
+    create_user_table(&pool).await?;
+
+    // Performing tests
+    testcase(&pool).await?;
+
+    test_all(&pool).await?;
+    test_list(&pool).await?;
+    test_info(&pool).await?;
+    // test_insert(&pool).await?;
+    test_insert2(&pool).await?;
+    test_update2(&pool).await?;
+    test_delete(&pool).await?;
+    test_batch_delete(&pool).await?;
+
+    test_all(&pool).await?;
+
+    Ok(())
+}
