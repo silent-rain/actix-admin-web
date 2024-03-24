305c8940e3749b9904401d645714d0f54911ddf0
silent-rain <2367221387@qq.com>
2024-03-24 15:31:54 +0800

    curd优化

--- server/core/database/src/curd.rs
+++ server/core/database/src/curd.rs
@@ -10,33 +10,39 @@ use sea_orm::{
 };
 
 #[async_trait]
-pub trait Curd<M>: Sync
+pub trait Curd<E>: Sync
 where
-    M: EntityTrait<Model = M> + FromQueryResult,
+    E: EntityTrait,
 {
     type Db: DbRepo;
 
     fn db(&self) -> &Self::Db;
 
-    /// 获取所有数据, 数据量过大时不推荐使用
-    async fn all(&self) -> Result<(Vec<M>, u64), DbErr> {
-        let result = M::find().all(self.db().rdb()).await?;
+    async fn all(&self) -> Result<(Vec<E::Model>, u64), DbErr>
+    where
+        E::Model: FromQueryResult,
+    {
+        let result = E::find().all(self.db().rdb()).await?;
         let total = result.len() as u64;
         Ok((result, total))
     }
 
     /// 获取数据列表
-    async fn list(&self, page: Pagination) -> Result<(Vec<M>, u64), DbErr> {
+    async fn list(&self, page: Pagination) -> Result<(Vec<E::Model>, u64), DbErr>
+    where
+        E::Model: FromQueryResult,
+    {
         // 获取总页数
-        // let pages: u64 = M::find()
+        // let pages: u64 = E::find()
         //     .paginate(self.db().rdb(), page.page_size())
         //     .num_items()
         //     .await?;
 
         // 获取总数
-        let total: u64 = M::find().paginate(self.db().rdb(), 1).num_items().await?;
+        // let total: u64 = E::find().paginate(self.db().rdb(), 1).num_items().await?;
+        let total: u64 = E::find().all(self.db().rdb()).await?.len() as u64;
 
-        let results = M::find()
+        let results = E::find()
             .offset(page.offset())
             .limit(page.page_size())
             .all(self.db().rdb())
@@ -45,36 +51,46 @@ where
     }
 
     /// 获取数据列表，对选择操作的结果进行分页
-    async fn list_pages(&self, page: Pagination) -> Result<(Vec<M>, u64), DbErr> {
-        let paginator = M::find().paginate(self.db().rdb(), page.page_size());
-        let num_pages = paginator.num_items().await?;
+    async fn _list_pages(&self, page: Pagination) -> Result<(Vec<E::Model>, u64), DbErr>
+    where
+        E::Model: FromQueryResult,
+        E: EntityTrait<Model = E>,
+    {
+        let paginator = E::find().paginate(self.db().rdb(), page.page_size());
+        let total_pages = paginator.num_items().await?;
 
         paginator
             .fetch_page(page.page())
             .await
-            .map(|results| (results, num_pages))
+            .map(|results| (results, total_pages))
     }
 
     /// 根据主键获取详情信息
-    async fn info(&self, id: i32) -> Result<Option<M>, DbErr>
+    async fn info(&self, id: i32) -> Result<Option<E::Model>, DbErr>
     where
-        <M::PrimaryKey as PrimaryKeyTrait>::ValueType: From<i32>,
+        <E::PrimaryKey as PrimaryKeyTrait>::ValueType: From<i32>,
     {
-        M::find_by_id(id).one(self.db().rdb()).await
+        E::find_by_id(id).one(self.db().rdb()).await
     }
 
     /// 插入数据
     /// 插入一个模型并返回一个新的 Model .其值是从数据库中检索的，因此将填充任何自动生成的字段。
-    async fn insert(
+    async fn _insert<A>(
         &self,
-        model: M,
-    ) -> Result<<<M as ActiveModelTrait>::Entity as EntityTrait>::Model, DbErr>
+        model: E,
+    ) -> Result<<<A as ActiveModelTrait>::Entity as EntityTrait>::Model, DbErr>
     where
-        <M::Entity as EntityTrait>::Model: IntoActiveModel<M>,
-        M: ActiveModelBehavior,
+        A: ActiveModelTrait
+            + ActiveModelBehavior
+            + std::marker::Send
+            + Into<A>
+            + std::convert::From<E>,
+        <<A as sea_orm::ActiveModelTrait>::Entity as sea_orm::EntityTrait>::Model:
+            IntoActiveModel<A>,
     {
-        let pear = model.into_active_model();
-        pear.insert(self.db().wdb()).await
+        let active_model: A = model.into();
+        let x = active_model.insert(self.db().wdb()).await;
+        x
     }
 
     /// 插入数据
@@ -84,7 +100,7 @@ where
         bean: A,
     ) -> Result<<<A as ActiveModelTrait>::Entity as EntityTrait>::Model, DbErr>
     where
-        A: ActiveModelTrait + ActiveModelBehavior + std::marker::Send,
+        A: ActiveModelTrait + ActiveModelBehavior + Send,
         <<A as ActiveModelTrait>::Entity as EntityTrait>::Model: IntoActiveModel<A>,
     {
         bean.insert(self.db().wdb()).await
@@ -94,11 +110,11 @@ where
     /// 接受一个模型并尝试更新数据库中的记录。
     async fn update(
         &self,
-        model: M,
-    ) -> Result<<<M as ActiveModelTrait>::Entity as EntityTrait>::Model, DbErr>
+        model: E,
+    ) -> Result<<<E as ActiveModelTrait>::Entity as EntityTrait>::Model, DbErr>
     where
-        <M::Entity as EntityTrait>::Model: IntoActiveModel<M>,
-        M: ActiveModelBehavior,
+        <E::Entity as EntityTrait>::Model: IntoActiveModel<E>,
+        E: ActiveModelBehavior,
     {
         let pear = model.into_active_model();
         pear.update(self.db().wdb()).await
@@ -120,19 +136,19 @@ where
     /// 按主键删除
     async fn delete(&self, id: i32) -> Result<u64, DbErr>
     where
-        <M::PrimaryKey as PrimaryKeyTrait>::ValueType: From<i32>,
+        <E::PrimaryKey as PrimaryKeyTrait>::ValueType: From<i32>,
     {
-        let result = M::delete_by_id(id).exec(self.db().wdb()).await?;
+        let result = E::delete_by_id(id).exec(self.db().wdb()).await?;
         Ok(result.rows_affected)
     }
 
     /// 按主键批量删除
     async fn batch_delete(&self, ids: Vec<i32>) -> Result<u64, DbErr>
     where
-        <M::PrimaryKey as PrimaryKeyTrait>::ValueType: From<i32>,
+        <E::PrimaryKey as PrimaryKeyTrait>::ValueType: From<i32>,
     {
-        let mut state = M::delete_many();
-        for key in M::PrimaryKey::iter() {
+        let mut state = E::delete_many();
+        for key in E::PrimaryKey::iter() {
             let col = key.into_column();
             state = state.filter(col.is_in(ids.clone()));
         }
