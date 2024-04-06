425e306593e2b032abcf4a9c587147b643a56569
silent-rain <2367221387@qq.com>
2024-04-06 21:39:32 +0800

    更新路由

--- server/crates/service_hub/src/template/router/template.rs
+++ server/crates/service_hub/src/template/router/template.rs
@@ -14,7 +14,7 @@ impl AppTemplateRouter {
             .route("", web::get().to(AppTemplateController::list))
             .route("/{id}", web::get().to(AppTemplateController::info))
             .route("", web::post().to(AppTemplateController::add))
-            .route("/{id}", web::put().to(AppTemplateController::update))
+            .route("/update", web::put().to(AppTemplateController::update))
             .route("/{id}/status", web::put().to(AppTemplateController::status))
             .route("/{id}", web::delete().to(AppTemplateController::delete))
             .route(
