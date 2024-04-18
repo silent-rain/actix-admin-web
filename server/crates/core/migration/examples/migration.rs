e012003aad1f20da150fe344be6136a51c1ca95b
silent-rain <2367221387@qq.com>
2024-04-18 20:05:51 +0800

    迁移数据库连接优化

--- server/crates/core/migration/examples/migration.rs
+++ server/crates/core/migration/examples/migration.rs
@@ -1,16 +1,13 @@
 //! 数据库库表迁移
 use std::env;
 
-use colored::Colorize;
-use database::{DbOptions, DbRepo};
+use database::DbOptions;
 use logger::config::{ConsoleConfig, Level, Logger};
-use migration::{Migrator, MigratorTrait};
 
+use colored::Colorize;
 use dotenv::dotenv;
+use migration::{Migrator, MigratorTrait};
 use tracing::{error, info};
-
-// const DATABASE_URL: &str = "sqlite://data.dat";
-// const DATABASE_URL: &str = "mysql://user:pass@127.0.0.1:3306/db_name";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -33,12 +30,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // 初始化数据库
     let database_url = env::var("DATABASE_URL").expect("read DATABASE_URL failed");
     let options = DbOptions::default();
-    let db = database::Pool::init(database_url.clone(), database_url, options)
+    let db = database::Pool::connect(database_url, options)
         .await
         .expect("初始化数据库失败");
 
     // 库表迁移器
-    if let Err(e) = Migrator::up(db.wdb(), None).await {
+    if let Err(e) = Migrator::up(&db, None).await {
         error!("表迁移失败. err: {e}");
     }
 
