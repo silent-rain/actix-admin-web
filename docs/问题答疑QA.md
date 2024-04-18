a264287ae9a9f44c4b740ef291008b03a2d4d454
silent-rain <2367221387@qq.com>
2024-04-18 20:50:55 +0800

    调整迁移数据库脚本,支持创建数据库

--- docs/问题答疑QA.md
+++ docs/问题答疑QA.md
@@ -16,7 +16,7 @@ pub struct JsonLayer {
 }
 ```
 
-## 将异步函数包装成同步函数
+## async_std 将异步函数包装成同步函数
 
 ```rust
 use async_std;
@@ -28,10 +28,64 @@ fn sync_task(&self, output: Model) {
 }
 ```
 
+## tokio+线程 将异步函数包装成同步函数
+
 ```rust
 fn sync_task(&self, output: Model) {
-    tokio::spawn(async move {
-        Self::save_db(output).await;
-    });
+std::thread::spawn(move || {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(async move {
+       // 异步代码
+    })
+})
+.join()
+.unwrap();
+}
+```
+
+## Sea-Orm 设置时区问题
+
+### 修改数据库配置
+
+在 MySQL 服务器端进行配置。
+在 MySQL 服务器的配置文件（通常是 my.cnf 或 my.ini）中设置默认时区。
+
+```text
+[mysqld]
+default-time-zone='+08:00'
+```
+
+### 在全局级别设置时区
+
+在全局级别设置时区。
+
+```sql
+SET GLOBAL time_zone = '+08:00';
+```
+
+### Sea-Orm 配置时区
+
+- 在 MySQL 的 JDBC 连接字符串中，通常使用 serverTimezone 参数来指定时区。
+- 然而，在 Rust 中，我们通常使用的是 mysql 或 mysql_async 这样的驱动程序，而不是 JDBC。
+- 对于 mysql 和 mysql_async 驱动程序，通常不需要在连接字符串中指定时区参数。这些库通常不支持在连接字符串中直接设置时区参数。通过执行 SQL 命令来设置会话的时区。
+
+```rust
+use sea_orm::{Database, DbErr};
+
+#[tokio::main]
+async fn main() -> Result<(), DbErr> {
+    let database_url = "mysql://username:password@localhost/dbname";
+    let db = Database::connect(database_url).await?;
+
+    // 设置会话时区为北京时间 (UTC+8)
+    db.execute(sea_orm::Statement::from_string(
+        db.get_database_backend(),
+        "SET time_zone = '+08:00'".to_owned(),
+    ))
+    .await?;
+
+    // ... 执行其他数据库操作 ...
+
+    Ok(())
 }
 ```
--- server/crates/core/migration/examples/migration.rs
+++ server/crates/core/migration/examples/migration.rs
@@ -7,7 +7,8 @@ use logger::config::{ConsoleConfig, Level, Logger};
 use colored::Colorize;
 use dotenv::dotenv;
 use migration::{Migrator, MigratorTrait};
-use tracing::{error, info};
+use sqlx::{mysql::MySqlPoolOptions, Executor};
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -29,6 +30,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // 初始化数据库
     let database_url = env::var("DATABASE_URL").expect("read DATABASE_URL failed");
+
+    // 数据库不存在则创建数据库
+    create_db(database_url.clone()).await?;
+
+    // 迁移表
+    migrator_up(database_url).await?;
+
+    info!("{}", "库表迁移完毕...".green());
+    Ok(())
+}
+
+/// 迁移表
+async fn migrator_up(database_url: String) -> Result<(), Box<dyn std::error::Error>> {
     let options = DbOptions::default();
     let db = database::Pool::connect(database_url, options)
         .await
@@ -36,9 +50,48 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // 库表迁移器
     if let Err(e) = Migrator::up(&db, None).await {
-        error!("表迁移失败. err: {e}");
+        panic!("表迁移失败. err: {e}");
     }
 
-    info!("{}", "库表迁移完毕...".green());
+    Ok(())
+}
+
+/// 数据库不存在则创建数据库
+async fn create_db(database_url: String) -> Result<(), Box<dyn std::error::Error>> {
+    // Sqlite3 无需创建数据库
+    if database_url.starts_with("sqlite") {
+        return Ok(());
+    }
+
+    // 数据库服务器连接信息
+    let slash_pos = database_url.rfind('/').unwrap();
+    let database_server = &database_url[0..slash_pos];
+    let database_name = &database_url[slash_pos + 1..];
+
+    // 创建数据库连接
+    let connection = MySqlPoolOptions::new().connect(database_server).await?;
+
+    // 检查数据库是否存在
+    let sql = format!(
+        "SELECT 1 FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '{}'",
+        database_name
+    );
+    let row: (Option<i32>,) = match sqlx::query_as(&sql).fetch_one(&connection).await {
+        Ok(v) => v,
+        Err(err) => match err {
+            sqlx::Error::RowNotFound => (None,),
+            _ => panic!("查询数据库异常, err:{:#?}", err),
+        },
+    };
+
+    // 如果数据库存在，则退出
+    if row.0.is_some() {
+        return Ok(());
+    }
+
+    connection
+    .execute(sqlx::query(&format!("CREATE DATABASE IF NOT EXISTS `{}` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci", database_name)))
+    .await?;
+
     Ok(())
 }
