1c1dd5ee903ef430a797e33d36d5d0dd6349a10e
silent-rain <2367221387@qq.com>
2024-04-19 20:01:33 +0800

    更新数据库连接参数文档

--- server/crates/core/database/src/pool.rs
+++ server/crates/core/database/src/pool.rs
@@ -35,10 +35,23 @@ impl Pool {
     }
 
     /// 连接数据库
+    /// 数据库连接参数:
+    /// 2. min_connections: 设置连接池的最小连接数。这是连接池会保持打开的最小数据库连接数量，即使这些连接当前没有被使用。
+    /// 3. connect_timeout: 设置连接数据库时的超时时间（以秒为单位）。如果在这段时间内无法建立连接，操作将被取消并返回错误。
+    /// 4. acquire_timeout: 设置从连接池获取连接时的超时时间（以秒为单位）。如果在这段时间内无法获取连接，操作将被取消并返回错误。
+    /// 5. idle_timeout: 设置连接在被回max_connections: 设置连接池的最大连接数。这是连接池可以打开的最大数据库连接数量。当达到这个数量时，新的连接收之前可以保持空闲状态的最长时间（以秒为单位）。如果连接在这段时间内没有被使用，它将被关闭并从连接池中移除。
+    /// 6. max_lifetime: 设置连接的最大生命周期（以秒为单位）。即使连接仍在使用中，超过这个时间后，它也会被关闭并从连接池中移除。
+    /// 7. sqlx_logging: 启用或禁用 SQLx 日志记录。SQLx 是 SeaORM 底层使用的数据库驱动，这个选项控制是否记录 SQLx 的日志信息。
+    /// 8. sqlx_logging_level: 设置 SQLx 日志记录的级别。这个选项决定了记录哪些级别的日志信息，例如错误、警告、信息或调试信息。
+    /// 
+    /// 查看最大连接数：
+    /// ```sql
+    /// SHOW VARIABLES LIKE 'max_connections';
+    /// ```
     pub async fn connect(db_url: String, options: DbOptions) -> Result<DatabaseConnection, Error> {
         let mut opt = ConnectOptions::new(db_url);
         opt.max_connections(options.max_connections)
-            .min_connections(options.max_connections)
+            .min_connections(options.min_connections)
             .connect_timeout(Duration::from_secs(options.connect_timeout))
             .acquire_timeout(Duration::from_secs(options.acquire_timeout))
             .idle_timeout(Duration::from_secs(options.idle_timeout))
